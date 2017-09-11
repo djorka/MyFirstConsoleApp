38dbdeaf476a8bc6963a983ce58eb026d4913733
andy schirmer <andrewschirmer3@gmail.com>
2017-09-11 16:09:47 -0600

    V 1.0

---
+++ FirstApp/FirstApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sName = "Andrew Schirmer";
+            int iCount;
+
+            for (iCount = 0; iCount < 3; iCount++)
+            {
+                Console.WriteLine(sName + " loves Fried SPAM!");
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
