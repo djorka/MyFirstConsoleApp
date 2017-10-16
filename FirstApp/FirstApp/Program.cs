fbf1b0f01bec97a3e0fa2b00ee5e37eef8427c7a
djorka <andrewschirmer3@gmail.com>
2017-10-16 09:01:18 -0600

    valid array to solve problem?

--- FirstApp/FirstApp/Program.cs
+++ FirstApp/FirstApp/Program.cs
@@ -8,11 +8,13 @@ namespace FirstApp
         {
             string sName = "Andrew";
             string sDog = "Roo";
+            int[] numbers = {1,2,3};
             int iCount;
 
             for (iCount = 0; iCount < 3; iCount++)
             {
                 Console.WriteLine(sName + " " + sDog + " loves Fried SPAM!");
+                Console.WriteLine(numbers[i])
             }
 
             Console.ReadLine();
